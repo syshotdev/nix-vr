fb0bb6e013da96130067e70e2935625da666f654
syshotdev <syshotdev@gmail.com>
2024-11-28 12:39:32 -0800

    Removed driSupport32Bit

--- modules/system/optimizations/gpu/default.nix
+++ modules/system/optimizations/gpu/default.nix
@@ -3,10 +3,11 @@
   hardware.opengl = {
     enable = true;
     driSupport = true;
-    driSupport32Bit = true;
+    # https://discourse.nixos.org/t/troubleshooting-steamvr/17406?u=syshotdev
+    #driSupport32Bit = true;
     # https://discourse.nixos.org/t/getting-an-error-has-anything-regarding-opengl-in-nixpkgs/3641/3
     setLdLibraryPath = true;
     # Mesa is general opengl drivers (I think)
-    extraPackages = [ pkgs.mesa.drivers ];
+    #extraPackages = [ pkgs.mesa.drivers ];
   };
 }
