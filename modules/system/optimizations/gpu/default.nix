5a170ac8f34fc036fc91f6ed5b04e8e09ecce755
syshotdev <syshotdev@gmail.com>
2024-11-29 20:17:34 -0800

    driSupport32Bit Opengl is probably needed

--- modules/system/optimizations/gpu/default.nix
+++ modules/system/optimizations/gpu/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     driSupport = true;
     # https://discourse.nixos.org/t/troubleshooting-steamvr/17406?u=syshotdev
-    #driSupport32Bit = true;
+    driSupport32Bit = true;
     # https://discourse.nixos.org/t/getting-an-error-has-anything-regarding-opengl-in-nixpkgs/3641/3
     setLdLibraryPath = true;
     # Mesa is general opengl drivers (I think)
