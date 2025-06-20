6a3988d0a6dcac8f8d051f1937d2251f1636ba5b
syshotdev <syshotdev@gmail.com>
2025-06-19 21:48:49 -0700

    Added steam options, did not help. Fixed with software encoding

--- modules/system/steam/default.nix
+++ modules/system/steam/default.nix
@@ -3,12 +3,18 @@
 # sudo setcap CAP_SYS_NICE+ep ~/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher
 # Extra args for steamvr launch: ~/.local/share/Steam/steamapps/common/SteamVR/bin/vrmonitor.sh %command% 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
   };
-
   # Extra dependencies for steamVR
   programs.steam.package = pkgs.steam.override {
     extraPkgs = pkgs: with pkgs; [ 
